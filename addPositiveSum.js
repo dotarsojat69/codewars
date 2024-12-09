719342d37987e975edd51a724329f8474e33362a
dotarsojat69 <tdeath457@gmail.com>
2024-12-09 16:25:53 +0700

    add note

--- addPositiveSum.js
+++ addPositiveSum.js
@@ -1,12 +1,15 @@
+// diberikan angka dalam array kembalikan penjumlahan nilai/angka positif nya
+
 function positiveSum(arr) {
-    let sum = 0;
-  
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > 0) {
-        sum += arr[i];
-      }
+  let sum = 0; // variabel untuk menampung hasil penjumlahan
+
+  for (let i = 0; i < arr.length; i++) {
+    // looping untuk memeriksa nilai positif dan negatif dalam array
+    if (arr[i] > 0) {
+      sum += arr[i]; // jika positif masukkan ke variabel jumlah
     }
-    return sum
   }
-  
-  console.log(positiveSum([-1,-2,-3,-4, 0, 9]))
+  return sum; // mengembalikan hasil penjumlahan
+}
+
+console.log(positiveSum([-1, -2, -3, -4, 0, 9]));
